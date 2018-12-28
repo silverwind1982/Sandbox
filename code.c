c6e0d179f0c3d94fa8ce43f24502b5dab638029a
silverwind1982 <fake@fake.com>
2018-12-28 08:56:04 +0800

    [Sandbox] fix and add

---
+++ code.c
@@ -0,0 +1,43 @@
+int ggg;
+
+int main() {
+  int k;
+
+  ggg = 3;
+  return 0;
+}
+
+int ttt() {
+  int k;
+
+  ggg = 8;
+  return 0;
+}
+
+int ab() {
+  int k;
+
+  ggg = 8;
+  return 0;
+}
+
+int abc() {
+  int k;
+
+  ggg = 8;
+  return 0;
+}
+
+int def() {
+  int k;
+
+  ggg = 28;
+  return 0;
+}
+
+int ghj() {
+  int k;
+
+  ggg = 38;
+  return 0;
+}
